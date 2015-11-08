b4f943c2abb30f0c2486be55b33c4218acf38d4c
Peter <peter@sitedisks.com.au>
2015-11-09 08:05:13 +1100

    required DTO service converter!

--- tohow.Service/TohowService.cs
+++ tohow.Service/TohowService.cs
@@ -5,12 +5,38 @@ using System.Text;
 using System.Threading.Tasks;
 using tohow.Interface.Service;
 using tohow.Data.Repository;
+using tohow.Domain.DTO;
+using tohow.Interface.Repository;
 
 namespace tohow.Service
 {
     public class TohowService: ITohowService
     {
+        private readonly ITohowDevRepository _reposTohowDev;
+
+        public TohowService(ITohowDevRepository repoTH) {
+            _reposTohowDev = repoTH;
+        }
         // use the repos and implement the interface function
-        
+        public Task<Image> GetImageByImageId(Guid imageId) {
+
+            Image img = new Image();
+
+            try {
+                var imgTT =  _reposTohowDev.GetImageById(imageId);
+
+
+                if (imgTT == null)
+                    throw new ApplicationException("Invalid Image!");
+
+                
+            }
+            catch(Exception ex)
+            {
+                throw new ApplicationException("Error retriving Image", ex);
+            }
+
+            return img;
+        }
     }
 }
