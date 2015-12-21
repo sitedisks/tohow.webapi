cc881f24c3e8ce0a5af833e86dc3a64d562ceeaa
Peter Wang <peter@sitedisks.com.au>
2015-12-21 16:29:32 +1100

    get user profile by email

--- tohow.API/Controllers/UserController.cs
+++ tohow.API/Controllers/UserController.cs
@@ -21,7 +21,8 @@ namespace tohow.API.Controllers
             if (!ModelState.IsValid)
                 return BadRequest(ModelState);
 
-            try {
+            try
+            {
                 var existUser = await _tohowSvc.GetUserProfileByEmail(req.Email);
                 if (existUser != null)
                     throw new ApplicationException("User Exist.");
@@ -36,31 +37,60 @@ namespace tohow.API.Controllers
             return Ok();
         }
 
-        [HttpGet, Route("userprofile/user")]
-        public async Task<IHttpActionResult> GetUerProfileByUserId(string userId) {
+        [HttpGet, Route("userprofile/byuser")]
+        public async Task<IHttpActionResult> GetUerProfileByUserId(string userId)
+        {
             UserProfileDetails userProfile = null;
 
-            try {
+            try
+            {
                 userProfile = await _tohowSvc.GetUserProfileByUserId(userId);
                 if (userProfile == null)
                     return NotFound();
             }
-            catch (ApplicationException aex) {
+            catch (ApplicationException aex)
+            {
                 return BadRequest(aex.Message);
             }
-            catch (Exception ex) {
+            catch (Exception ex)
+            {
                 return InternalServerError(ex);
             }
 
             return Ok(userProfile);
         }
 
-        [HttpGet, Route("userprofile/profile")]
-        public async Task<IHttpActionResult> GetUserProfileByProfileId(int profileId) {
+        [HttpGet, Route("userprofile/byprofile")]
+        public async Task<IHttpActionResult> GetUserProfileByProfileId(int profileId)
+        {
             UserProfileDetails userProfile = null;
 
-            try {
+            try
+            {
                 userProfile = await _tohowSvc.GetUserProfileByProfileId(profileId);
+                if (userProfile == null)
+                    return NotFound();
+            }
+            catch (ApplicationException aex)
+            {
+                return BadRequest(aex.Message);
+            }
+            catch (Exception ex)
+            {
+                return InternalServerError(ex);
+            }
+
+            return Ok(userProfile);
+        }
+
+        [HttpGet, Route("userprofile/byemail")]
+        public async Task<IHttpActionResult> GetUserProfileByEmail(string email)
+        {
+            UserProfileDetails userProfile = null;
+
+            try
+            {
+                userProfile = await _tohowSvc.GetUserProfileByEmail(email);
                 if (userProfile == null)
                     return NotFound();
             }
